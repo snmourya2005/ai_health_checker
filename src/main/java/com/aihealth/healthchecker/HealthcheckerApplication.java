441355728f76df4e434fefa1d4562c5da5c0bbd9
snmourya2005 <s.n.mourya2005@gmail.com>
2026-01-10 22:29:24 +0530

    Initial Spring Boot setup with MySQL

---
+++ src/main/java/com/aihealth/healthchecker/HealthcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.aihealth.healthchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aihealth/healthchecker/HealthcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aihealth.healthchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
